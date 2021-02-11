25918d62a6bd03b6809dcf5f747c221accf62fd4
Leonardo Manzella <leonardo.manzella@gmail.com>
2021-02-11 12:48:27 -0300

    Using CDN for augmented-ui lib since it brokes build or brokes styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,9 +12,6 @@ import "prismjs/themes/prism.css"
 // Import tailwind styles
 import './src/global.css';
 
-// Import augmented-ui lib
-import './src/augmented-ui-1.1.2.css';
-
 export const onServiceWorkerUpdateReady = () => {
     const answer = window.confirm(
         `This application has been updated. ` +
